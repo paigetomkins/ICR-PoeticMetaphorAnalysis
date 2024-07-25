5a09d0901676111d5e7ff3d90da938f033adb729
Paige Tomkins <ptomkins63@gmail.com>
2024-07-25 13:44:39 -0600

    Changed my absolute files paths to relative file paths.

--- All-Code/exploratory-data-analysis.py
+++ All-Code/exploratory-data-analysis.py
@@ -1,22 +1,63 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pylab as plt
-import seaborn as sns
+
 plt.style.use('ggplot')
 pd.options.display.max_columns = 6
 
-df = pd.read_csv('/home/paige-tomkins/PoeticMetaphorAnalysis/fulldataset.csv')
+df = pd.read_csv('../fulldataset.csv')
 
-#Distributions
+#Basic info
+
+numauthors = df.drop_duplicates(subset=['author']).count()
+print("Number of authors: ", numauthors)
+
+#Null Values:
+
+print("Missing values:")
+
+authordf = df[['author']].copy()
+nullauthor = authordf.isna().sum()
+print("missing", nullauthor)
+
+typedf = df[['type']].copy()
+nulltype = typedf.isna().sum()
+print("missing", nulltype)
+
+titledf = df[['poem name']].copy()
+nulltitle = titledf.isna().sum()
+print("missing", nulltitle)
+
+#Duplicated Values:
+
+duplepoem = df.duplicated(subset=['poem name']).sum()
+print("Duplicated poems:", duplepoem)
+
+
+
+
 """
+#Graph: Age Distribution
+
 ax = df['age'].value_counts().plot(kind='bar', title='Age Distribution')
 
 ax.set_xlabel('Age')
 ax.set_ylabel('Number of Poems Included')
 plt.show()
+
+#Graph: Authorship Distribution
+
+ax = df['author'].value_counts().plot(kind='bar', title='Authorship Distribution')
+
+ax.set_xlabel('Author Name')
+ax.set_ylabel('Number of Poems Included')
+plt.show()
+
+#Graph: Thematic Distribution
+
+ax = df['type'].value_counts().plot(kind='bar', title='Thematic Distribution')
+
+ax.set_xlabel('Theme')
+ax.set_ylabel('Number of Poems Included')
+plt.show()
 """
-
-#Correlation
-correlation = df[['author', 'age']].corr()
-print(correlation)
-
--- All-Code/import-data-as-dictlist.py
+++ All-Code/import-data-as-dictlist.py
@@ -1,12 +1,10 @@
 import csv
 
-f = open('/home/paige-tomkins/ICR-linguistics/all.csv', 'r')
+f = open('../fulldataset.csv', 'r')
 
 reader = csv.DictReader(f)
 
 poems = list(reader)
 
-#for dictionary in reader:
- #   poems.append(dictionary)
 print("The list of dictionaries is: ")
 print(poems)
